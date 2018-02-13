0a7607e05de22e1c00de21a40fb9154fcfd6dd31
Sam Meech-Ward <sam@meech-ward.com>
2018-02-13 09:06:29 -0800

    Initial Commit

---
+++ JazzHands/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JazzHands
+//
+//  Created by Sam Meech-Ward on 2018-02-13.
+//  Copyright © 2018 lighthouse-labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
